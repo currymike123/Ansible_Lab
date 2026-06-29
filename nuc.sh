301b4d13d303757cf9412cf6f7b83ca2f2808e08
currymike123 <currymike@gmail.com>
2026-06-29 11:35:11 -0400

    Update for sudo password

--- nuc.sh
+++ nuc.sh
@@ -7,7 +7,7 @@
 # Check if the first argument passed to the script is "setup"
 if [ "$1" == "setup" ]; then
     echo "Running the NUC setup playbook..."
-    ansible-playbook -i inventory.ini setup_lab.yml --ask-vault-pass --ask-pass
+    ansible-playbook -i inventory.ini setup_lab.yml --ask-vault-pass --ask-pass --ask-become-pass
 else
     echo "Usage: ./nuc setup" # Updated to reflect the conventional name
     exit 1
