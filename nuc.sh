b316937a77360fc67d2f4a103db7f58db374f52d
currymike123 <currymike@gmail.com>
2026-06-29 12:20:29 -0400

    Remove the user for the inventory file

--- nuc.sh
+++ nuc.sh
@@ -7,10 +7,10 @@
 # Check if the first argument passed to the script is "setup"
 if [[ "$1" == "setup" ]]; then
     echo "Running the NUC setup playbook..."
-    ansible-playbook -i inventory.ini setup_lab.yml --ask-vault-pass --ask-pass --ask-become-pass
+    ansible-playbook -i inventory.ini setup_lab.yml --ask-vault-pass --ask-pass --ask-become-pass -e ansible_user=test
 elif [[ "$1" == "reset" ]]; then
     echo "Running the NUC reset playbook..."
-    ansible-playbook -i inventory.ini reset_lab.yml --ask-vault-pass --ask-pass --ask-become-pass
+    ansible-playbook -i inventory.ini reset_lab.yml --ask-vault-pass --ask-pass --ask-become-pass -e ansible_user=csadmin
 else
     echo "Usage: $0 <command>"
     echo "Commands:"
